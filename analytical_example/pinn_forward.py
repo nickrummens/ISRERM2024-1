eb63dcf57c0184421db633288bb7ed4c24310932
bonneted <bonneteymard.damien@gmail.com>
2024-11-05 10:32:46 +0100

    init analytical example

---
+++ analytical_example/pinn_forward.py
@@ -0,0 +1,335 @@
+"""
+PINN and SPINN implementation of the linear elasticity 2D toy example introduced in paper https://doi.org/10.1016/j.cma.2021.113741.
+"""
+
+import deepxde as dde
+import numpy as np
+import time
+import os
+
+net_type = ["spinn", "pfnn"][0] # SPINN only implemented with JAX backend and HardBC
+
+n_iter = int(1e6) # Large number, will be stopped by the Timer callback
+log_every = 100
+available_time = [False, 2][1] #minutes
+log_output_fields = {0: "Ux", 1: "Uy"}  # 2: "Sxx", 3: "Syy", 4: "Sxy"}
+bc_type = ["hard", "soft"][0]
+mlp = ["mlp", "modified_mlp"][0]
+
+if net_type == "spinn":
+    dde.config.set_default_autodiff("forward")
+
+lmbd = 1.0
+mu = 0.5
+Q = 4.0
+
+sin = dde.backend.sin
+cos = dde.backend.cos
+stack = dde.backend.stack
+pi = dde.backend.as_tensor(np.pi)
+
+if dde.backend.backend_name == "jax":
+    import jax.numpy as jnp
+
+geom = dde.geometry.Rectangle([0, 0], [1, 1])
+
+
+def boundary_left(x, on_boundary):
+    return on_boundary and dde.utils.isclose(x[0], 0.0)
+
+
+def boundary_right(x, on_boundary):
+    return on_boundary and dde.utils.isclose(x[0], 1.0)
+
+
+def boundary_top(x, on_boundary):
+    return on_boundary and dde.utils.isclose(x[1], 1.0)
+
+
+def boundary_bottom(x, on_boundary):
+    return on_boundary and dde.utils.isclose(x[1], 0.0)
+
+
+# Exact solutions
+def func(x):
+    if net_type == "spinn":
+        """For SPINN, the input x is a list of 1D arrays (X_coords, Y_coords)
+        that need to be converted to a 2D meshgrid of same shape as the ouput"""
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[0].squeeze(), x[1].squeeze(), indexing="ij")]
+        x = stack(x_mesh, axis=-1)
+
+    ux = np.cos(2 * np.pi * x[:, 0:1]) * np.sin(np.pi * x[:, 1:2])
+    uy = np.sin(np.pi * x[:, 0:1]) * Q * x[:, 1:2] ** 4 / 4
+
+    E_xx = -2 * np.pi * np.sin(2 * np.pi * x[:, 0:1]) * np.sin(np.pi * x[:, 1:2])
+    E_yy = np.sin(np.pi * x[:, 0:1]) * Q * x[:, 1:2] ** 3
+    E_xy = 0.5 * (
+        np.pi * np.cos(2 * np.pi * x[:, 0:1]) * np.cos(np.pi * x[:, 1:2])
+        + np.pi * np.cos(np.pi * x[:, 0:1]) * Q * x[:, 1:2] ** 4 / 4
+    )
+
+    Sxx = E_xx * (2 * mu + lmbd) + E_yy * lmbd
+    Syy = E_yy * (2 * mu + lmbd) + E_xx * lmbd
+    Sxy = 2 * E_xy * mu
+
+    return np.hstack((ux, uy, Sxx, Syy, Sxy))
+
+
+ux_top_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_top, component=0)
+ux_bottom_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_bottom, component=0)
+uy_left_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_left, component=1)
+uy_bottom_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_bottom, component=1)
+uy_right_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_right, component=1)
+sxx_left_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_left, component=2)
+sxx_right_bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary_right, component=2)
+syy_top_bc = dde.icbc.DirichletBC(
+    geom,
+    lambda x: (2 * mu + lmbd) * Q * np.sin(np.pi * x[:, 0:1]),
+    boundary_top,
+    component=3,
+)
+
+
+def HardBC(x, f):
+    if net_type == "spinn" and isinstance(x, list):
+        """For SPINN, the input x is a list of 1D arrays (X_coords, Y_coords)
+        that need to be converted to a 2D meshgrid of same shape as the output f"""
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[0].squeeze(), x[1].squeeze(), indexing="ij")]
+        x = stack(x_mesh, axis=-1)
+
+    Ux = f[:, 0] * x[:, 1] * (1 - x[:, 1])
+    Uy = f[:, 1] * x[:, 0] * (1 - x[:, 0]) * x[:, 1]
+
+    Sxx = f[:, 2] * x[:, 0] * (1 - x[:, 0])
+    Syy = f[:, 3] * (1 - x[:, 1]) + (lmbd + 2 * mu) * Q * sin(pi * x[:, 0])
+    Sxy = f[:, 4]
+    return stack((Ux, Uy, Sxx, Syy, Sxy), axis=1)
+
+
+def fx(x):
+    return (
+        -lmbd
+        * (
+            4 * np.pi**2 * cos(2 * np.pi * x[:, 0:1]) * sin(np.pi * x[:, 1:2])
+            - Q * x[:, 1:2] ** 3 * np.pi * cos(np.pi * x[:, 0:1])
+        )
+        - mu
+        * (
+            np.pi**2 * cos(2 * np.pi * x[:, 0:1]) * sin(np.pi * x[:, 1:2])
+            - Q * x[:, 1:2] ** 3 * np.pi * cos(np.pi * x[:, 0:1])
+        )
+        - 8 * mu * np.pi**2 * cos(2 * np.pi * x[:, 0:1]) * sin(np.pi * x[:, 1:2])
+    )
+
+
+def fy(x):
+    return (
+        lmbd
+        * (
+            3 * Q * x[:, 1:2] ** 2 * sin(np.pi * x[:, 0:1])
+            - 2 * np.pi**2 * cos(np.pi * x[:, 1:2]) * sin(2 * np.pi * x[:, 0:1])
+        )
+        - mu
+        * (
+            2 * np.pi**2 * cos(np.pi * x[:, 1:2]) * sin(2 * np.pi * x[:, 0:1])
+            + (Q * x[:, 1:2] ** 4 * np.pi**2 * sin(np.pi * x[:, 0:1])) / 4
+        )
+        + 6 * Q * mu * x[:, 1:2] ** 2 * sin(np.pi * x[:, 0:1])
+    )
+
+
+def jacobian(f, x, i, j):
+    if dde.backend.backend_name == "jax":
+        return dde.grad.jacobian(f, x, i=i, j=j)[0]  # second element is the function used by jax to compute the gradients
+    else:
+        return dde.grad.jacobian(f, x, i=i, j=j)
+
+
+def pde(x, f):
+    if net_type == "spinn":
+        """For SPINN, the input x is a list of 1D arrays (X_coords, Y_coords)
+        that need to be converted to a 2D meshgrid of same shape as the output f"""
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[0].squeeze(), x[1].squeeze(), indexing="ij")]
+        x = stack(x_mesh, axis=-1)
+
+    E_xx = jacobian(f, x, i=0, j=0)
+    E_yy = jacobian(f, x, i=1, j=1)
+    E_xy = 0.5 * (jacobian(f, x, i=0, j=1) + jacobian(f, x, i=1, j=0))
+
+    S_xx = E_xx * (2 * mu + lmbd) + E_yy * lmbd
+    S_yy = E_yy * (2 * mu + lmbd) + E_xx * lmbd
+    S_xy = E_xy * 2 * mu
+
+    Sxx_x = jacobian(f, x, i=2, j=0)
+    Syy_y = jacobian(f, x, i=3, j=1)
+    Sxy_x = jacobian(f, x, i=4, j=0)
+    Sxy_y = jacobian(f, x, i=4, j=1)
+
+    momentum_x = Sxx_x + Sxy_y - fx(x)
+    momentum_y = Sxy_x + Syy_y - fy(x)
+
+    if dde.backend.backend_name == "jax":
+        f = f[0]  # f[1] is the function used by jax to compute the gradients
+
+    stress_x = S_xx - f[:, 2:3]
+    stress_y = S_yy - f[:, 3:4]
+    stress_xy = S_xy - f[:, 4:5]
+
+    return [momentum_x, momentum_y, stress_x, stress_y, stress_xy]
+
+
+if bc_type == "hard":
+    bcs = []
+    num_boundary = 0
+else:
+    bcs = [
+        ux_top_bc,
+        ux_bottom_bc,
+        uy_left_bc,
+        uy_bottom_bc,
+        uy_right_bc,
+        sxx_left_bc,
+        sxx_right_bc,
+        syy_top_bc,
+    ]
+    num_boundary = 64 if net_type == "spinn" else 500
+
+
+def get_num_params(net, input_shape=None):
+    if dde.backend.backend_name == "pytorch":
+        return sum(p.numel() for p in net.parameters())
+    elif dde.backend.backend_name == "paddle":
+        return sum(p.numpy().size for p in net.parameters())
+    elif dde.backend.backend_name == "jax":
+        if input_shape is None:
+            raise ValueError("input_shape must be provided for jax backend")
+        import jax
+        import jax.numpy as jnp
+
+        rng = jax.random.PRNGKey(0)
+        return sum(
+            p.size for p in jax.tree.leaves(net.init(rng, jnp.ones(input_shape)))
+        )
+
+
+activation = "tanh"
+initializer = "Glorot uniform"
+optimizer = "adam"
+if net_type == "spinn":
+    layers = [2, 32, 32, 32, 32, 5]
+    net = dde.nn.SPINN(layers, activation, initializer, mlp)
+    num_point = 64**2
+    total_points = num_point 
+    num_params = get_num_params(net, input_shape=layers[0])
+    X_plot = [np.linspace(0, 1, 100).reshape(-1, 1)] * 2 #SPINN takes list of 1D arrays (X_coords, Y_coords) as input
+
+else:
+    layers = [2, [40] * 5, [40] * 5, [40] * 5, [40] * 5, 5]
+    net = dde.nn.PFNN(layers, activation, initializer)
+    num_point = 500
+    total_points = num_point + num_boundary
+    num_params = get_num_params(net, input_shape=layers[0])
+    X_mesh = np.meshgrid(
+        np.linspace(0, 1, 100, dtype=np.float32),
+        np.linspace(0, 1, 100, dtype=np.float32),
+        indexing="ij",
+    )
+    X_plot = np.stack((X_mesh[0].ravel(), X_mesh[1].ravel()), axis=1)
+
+
+data = dde.data.PDE(
+    geom,
+    pde,
+    bcs,
+    num_domain=num_point,
+    num_boundary=num_boundary,
+    solution=func,
+    num_test=num_point,
+    is_SPINN=net_type == "spinn",
+)
+
+if bc_type == "hard":
+    net.apply_output_transform(HardBC)
+
+# Saving the results
+folder_name = f"{net_type}_{available_time if available_time else n_iter}{'min' if available_time else 'iter'}"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+results_path = os.path.join(dir_path, "results_forward")
+
+# Check if any folders with the same name exist
+existing_folders = [f for f in os.listdir(results_path) if f.startswith(folder_name)]
+
+# If there are existing folders, find the highest number suffix
+if existing_folders:
+    suffixes = [int(f.split("-")[-1]) for f in existing_folders if f != folder_name]
+    if suffixes:
+        max_suffix = max(suffixes)
+        folder_name = f"{folder_name}-{max_suffix + 1}"
+    else:
+        folder_name = f"{folder_name}-1"
+
+# Create the new folder
+new_folder_path = os.path.join(results_path, folder_name)
+if not os.path.exists(new_folder_path):
+    os.makedirs(new_folder_path)
+
+callbacks = [dde.callbacks.Timer(available_time)] if available_time else []
+
+callbacks = [dde.callbacks.Timer(available_time)] if available_time else []
+for i, field in log_output_fields.items():
+    output_op = lambda x, output, i=i: output[0][:, i]
+    callbacks.append(dde.callbacks.OperatorPredictor(X_plot, output_op, period=log_every, filename=os.path.join(new_folder_path, f"{field}_history.dat")))
+
+
+model = dde.Model(data, net)
+model.compile(optimizer, lr=0.001, metrics=["l2 relative error"])
+
+start_time = time.time()
+losshistory, train_state = model.train(
+    iterations=n_iter, callbacks=callbacks, display_every=log_every
+)
+elapsed = time.time() - start_time
+
+
+def log_config(fname):
+    import json
+    import platform
+    import psutil
+
+    system_info = {
+        "OS": platform.system(),
+        "Release": platform.release(),
+        "Version": platform.version(),
+        "Machine": platform.machine(),
+        "Processor": platform.processor(),
+        "CPU count": psutil.cpu_count(),
+        "RAM": psutil.virtual_memory().total / (1024**3),
+    }
+
+    execution_info = {
+        "n_iter": train_state.epoch,
+        "elapsed": elapsed,
+        "iter_per_sec": train_state.epoch / elapsed,
+        "backend": dde.backend.backend_name,
+        "batch_size": total_points,
+        "num_params": num_params,
+        "activation": activation,
+        "initializer": initializer,
+        "optimizer": optimizer,
+        "net_type": net_type,
+        "mlp": mlp,
+        "bc_type": bc_type,
+        "logged_fields": log_output_fields,
+    }
+
+    info = {**system_info, **execution_info}
+    info_json = json.dumps(info, indent=4)
+
+    with open(fname, "w") as f:
+        f.write(info_json)
+
+
+log_config(os.path.join(new_folder_path, "config.json"))
+dde.utils.save_loss_history(
+    losshistory, os.path.join(new_folder_path, "loss_history.dat")
+)
